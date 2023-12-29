83a0d870467cf40407158d885521ddcdd196ed33
Kirill <kz@selfiemosaic.ru>
2023-12-29 15:32:47 +0300

    Task_4 complete

---
+++ Task_4/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 4: Напишите программу, которая на вход
+// принимает натуральное число N, а на выходе
+// показывает его цифры через запятую.
+
+Console.WriteLine("Введите целое двухзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int numCut = number;
+int i = 10;
+
+if (number < 0)
+{
+    Console.WriteLine("Вы ввели ненатуральное число");
+}
+else
+{
+    if ((number / 10) == 0)
+    {
+        Console.WriteLine(number);
+    }
+    else
+    {   
+        while (numCut > 9)
+        {
+            i = (i * 10);
+            numCut = (number / i);
+
+        }
+        // Console.WriteLine(i);
+    }
+    Console.Write($"{number} =>");
+    while(i > 0)
+    {
+        numCut = (number / i);
+        number = (number % i);
+        i = (i / 10);
+
+        Console.Write($"{numCut}, ");
+
+    }
+    
+}
