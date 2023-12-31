e8e740f130fd419429f1e990164ac79a277dd4d0
Kirill <kz@selfiemosaic.ru>
2023-12-31 13:00:21 +0300

    Edit Task4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -6,18 +6,35 @@ Console.WriteLine("Введите натуральное число");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int numCut = number;
+int numCutTemp = number;
 int range = (Convert.ToInt32(number.ToString().Length) - 1);
 int i = Convert.ToInt32(Math.Pow(10,range));
 int firstNum = number / i;
 string text1 = number.ToString(number + "==> " + firstNum.ToString());
 string text2 = text1;
+//Console.WriteLine(i);
 
-i = i / 10;
+//i = i / 10;
+//Console.WriteLine(i);
+//Console.WriteLine(number);
+numCut = number % i;
+//Console.WriteLine(numCut);
+numCutTemp = numCut;
+//Console.WriteLine(numCutTemp);
+//i = i / 10;
+
+
 while(i > 1)
 {
-    numCut = number % (i);
-    text2 = text2 + ", " + numCut.ToString();
+    numCutTemp = numCutTemp % i;
+    
+    numCut = numCutTemp/ (i / 10);
+       
+    //Console.WriteLine(numCut);
+    
     i = i / 10;
+    text2 = text2 + ", " + numCut.ToString();
+ 
 }
 
 // string text = number;
