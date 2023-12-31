d7187c5c28069f38ca31b36c21e7b60374e392fe
Kirill <kz@selfiemosaic.ru>
2023-12-31 05:05:05 +0300

    Rework Task4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -2,40 +2,63 @@
 // принимает натуральное число N, а на выходе
 // показывает его цифры через запятую.
 
-Console.WriteLine("Введите целое двухзначное число");
+Console.WriteLine("Введите натуральное число");
 int number = Convert.ToInt32(Console.ReadLine());
+
 int numCut = number;
-int i = 10;
+int range = (Convert.ToInt32(number.ToString().Length) - 1);
+int i = Convert.ToInt32(Math.Pow(10,range));
+int firstNum = number / i;
+string text1 = number.ToString(number + "==> " + firstNum.ToString());
+string text2 = text1;
 
-if (number < 0)
+i = i / 10;
+while(i > 1)
 {
-    Console.WriteLine("Вы ввели ненатуральное число");
+    numCut = number % (i);
+    text2 = text2 + ", " + numCut.ToString();
+    i = i / 10;
 }
-else
-{
-    if ((number / 10) == 0)
-    {
-        Console.WriteLine(number);
-    }
-    else
-    {   
-        while (numCut > 9)
-        {
-            i = (i * 10);
-            numCut = (number / i);
 
-        }
-        // Console.WriteLine(i);
-    }
-    Console.Write($"{number} =>");
-    while(i > 0)
-    {
-        numCut = (number / i);
-        number = (number % i);
-        i = (i / 10);
+// string text = number;
 
-        Console.Write($"{numCut}, ");
+Console.WriteLine(text2);
+//Console.WriteLine(i);
 
-    }
+// while (i > 0)
+// {
+
+// }
+
+// if (number < 0)
+// {
+//     Console.WriteLine("Вы ввели ненатуральное число");
+// }
+// else
+// {
+//     if ((number / 10) == 0)
+//     {
+//         Console.WriteLine(number);
+//     }
+//     else
+//     {   
+//         while (numCut > 9)
+//         {
+//             i = (i * 10);
+//             numCut = (number / i);
+
+//         }
+//         // Console.WriteLine(i);
+//     }
+//     Console.Write($"{number} =>");
+//     while(i > 0)
+//     {
+//         numCut = (number / i);
+//         number = (number % i);
+//         i = (i / 10);
+
+//         Console.Write($"{numCut}, ");
+
+//     }
     
-}
+// }
